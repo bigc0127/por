a2d6b20b156373457de78a1eb5e333e4d51341fb
Connor Needling <39162958+bigc0127@users.noreply.github.com>
2023-01-13 15:54:14 -0500

    Update por-installer.sh

--- por-installer.sh
+++ por-installer.sh
@@ -12,18 +12,20 @@ if ! command -v por &> /dev/null
     chmod +x ./por
     mv -v ./por ~/.utils
     echo 'export PATH="$HOME/.utils:$PATH"' >> ~/.bashrc
-    if ! command -v fish &> /dev/null
-      then
-       echo "fish not installed not adding to fish.conf"
-      else
-       echo 'export PATH="$HOME/.utils:$PATH"' >> ~/.config/fish/config.fish
-   fi
-   if ! command -v zsh &> /dev/null
-      then
-        echo "zsh not installed, not adding to .zshrc"
-      else
-        echo 'export PATH="$HOME/.utils:$PATH"' >> ~/.zshrc
-   fi
   else
     mv -v ./por ~/.utils
+fi
+
+if ! command -v fish &> /dev/null
+ then
+    echo "fish not installed not adding to fish.conf"
+ else
+    echo 'export PATH="$HOME/.utils:$PATH"' >> ~/.config/fish/config.fish
+fi
+
+if ! command -v zsh &> /dev/null
+ then
+  echo "zsh not installed, not adding to .zshrc"
+ else
+  echo 'export PATH="$HOME/.utils:$PATH"' >> ~/.zshrc
 fi
