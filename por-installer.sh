6f0ceaba3ca5b89c882443393c922495af89b09e
Connor Needling <39162958+bigc0127@users.noreply.github.com>
2023-01-13 16:05:07 -0500

    Update por-installer.sh

--- por-installer.sh
+++ por-installer.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-if  ! -e por
-  then
+FILE=./por
+if test -f "$FILE"; then
      echo "installing local copy of por"
   else
      curl -o por https://raw.githubusercontent.com/bigc0127/por/main/por
 fi
 
-if ! command -v por &> /dev/null
-  then
+if command -v por &> /dev/null; then
     mkdir ~/.utils
     chmod +x ./por
     mv -v ./por ~/.utils
     echo 'export PATH="$HOME/.utils:$PATH"' >> ~/.bashrc
-    if ! command -v fish &> /dev/null
-      then
-       echo "fish not installed not adding to fish.conf"
-      else
-       echo 'export PATH="$HOME/.utils:$PATH"' >> ~/.config/fish/config.fish
-   fi
-   if ! command -v zsh &> /dev/null
-      then
-        echo "zsh not installed, not adding to .zshrc"
-      else
-        echo 'export PATH="$HOME/.utils:$PATH"' >> ~/.zshrc
-   fi
   else
     mv -v ./por ~/.utils
+fi
+
+if command -v fish &> /dev/null; then
+    echo "fish not installed not adding to fish.conf"
+ else
+    echo 'export PATH="$HOME/.utils:$PATH"' >> ~/.config/fish/config.fish
+fi
+
+if command -v zsh &> /dev/null; then
+  echo "zsh not installed, not adding to .zshrc"
+ else
+  echo 'export PATH="$HOME/.utils:$PATH"' >> ~/.zshrc
 fi
